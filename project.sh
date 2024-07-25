53d89e9fa9313a40ad20b84cfecfe8563df78d00
pubg4942 <122099467+pubg4942@users.noreply.github.com>
2024-07-25 18:17:03 +0530

    Update project.sh

--- project.sh
+++ project.sh
@@ -1,15 +1,15 @@
-if [ -d \source ]; then
+if [ -d C:\Users\Regenerate\Desktop\hardwork\source ]; then
     echo "source folder present"
-    if [ -d \destination ]; then
+    if [ -d C:\Users\Regenerate\Desktop\hardwork\destination ]; then
         echo "destination folder present"
         echo "copying from source to destination"
-        cp \source\/* \destination
+        cp C:\Users\Regenerate\Desktop\hardwork\source\/* C:\Users\Regenerate\Desktop\hardwork\destination
     else
         echo "destination folder does not exit"
         echo "creating destination folder"
         mkdir destination
         echo "copying from source to destination"
-        cp \source\/* \destination
+        cp C:\Users\Regenerate\Desktop\hardwork\source\/* C:\Users\Regenerate\Desktop\hardwork\destination
     fi
 else
     echo "source not available hence exiting "
