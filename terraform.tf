b7bb4366bd8d6d74235e3bc5cc2fce9d34e7c26b
pubg4942 <122099467+pubg4942@users.noreply.github.com>
2024-08-01 14:41:48 +0530

    Update terraform.tf

--- terraform.tf
+++ terraform.tf
@@ -1,7 +1,43 @@
-variable "input" {
-    default = "helloworld"
+variable "list" {
+    default = ["hello", "world", "how", "are", "you" ]
 }
 
-output "input" {
-    value = var.input
+variable "map" {
+  default = {
+    name = "test",
+    env = "dev",
+    region = "us-west-2"
+  }
+}
+
+variable "string" {
+    default = "hello_world"
+}
+
+variable "boolean" {
+  default = true
+}
+
+variable "number" {
+  default = 5
+}
+
+output "list" {
+  value = var.list
+}
+
+output "map" {
+    value = var.map
+}
+
+output "string" {
+  value = var.string
+}
+
+output "boolean" {
+    value = var.boolean
+}
+
+output "number" {
+  value = var.number
 }
